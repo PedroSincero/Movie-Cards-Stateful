e9ab0fd1f8339bb92d9e922e12047c7fcb9df16b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 17:55:33 -0300

    adicionando Desafio 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,8 +6,8 @@ class SearchBar extends Component {
     const {
       searchText,
       onSearchTextChange,
-      //   bookmarkedOnly,
-      //   onBookmarkedChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
       //   selectedGenre,
       // onSelectedGenreChange,
     } = this.props;
@@ -22,6 +22,16 @@ class SearchBar extends Component {
             data-testid="text-input"
           />
         </label>
+
+        <label htmlFor="checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="checkbox"
+            checked={ bookmarkedOnly }
+            onChange={ onBookmarkedChange } data-testid="checkbox-input"
+          />
+        </label>
       </form>
     );
   }
@@ -30,6 +40,8 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
