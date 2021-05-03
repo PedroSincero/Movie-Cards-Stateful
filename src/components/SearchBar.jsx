a5281a39a926d58a41dbe0618ce6d0f07836210f
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 15:44:16 -0300

    adicionando coisas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
