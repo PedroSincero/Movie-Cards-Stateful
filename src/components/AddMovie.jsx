395b08ccd66658c73c02d55c834ac5b05641cb0b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 18:25:43 -0300

    adicionando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class AddMovies extends Component {
-
+  render() {
+    return (
+      <form>
+        <input />
+      </form>
+    );
+  }
 }
 
 export default AddMovies;
