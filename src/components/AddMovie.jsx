37f6c70764fccfc2b292c5ca0252ab25739a1d8d
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 18:59:53 -0300

    adicionando 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class AddMovies extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
-      <form>
+      <form data-testid="add-movie-form">
         <input />
       </form>
     );
