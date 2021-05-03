e6879c72ac7302d369c4fa1575d591aa2d322b92
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 17:36:55 -0300

    adicionando coisas

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    // const {
-    // searchText,
-    // onSearchTextChange,
-    // bookmarkedOnly,
-    // onBookmarkedChange,
-    // selectedGenre,
-    // onSelectedGenreChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      //   bookmarkedOnly,
+      //   onBookmarkedChange,
+      //   selectedGenre,
+      // onSelectedGenreChange,
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <input />
+        <label htmlFor="input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            id="input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
