8472601b882f6814b64927ac5ad4cada64f977af
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-03 16:13:38 -0300

    adicionando Desafio 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
-// import MovieList from './MovieList';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   // constructor(props) {
@@ -10,6 +10,7 @@ class MovieLibrary extends Component {
   // }
 
   render() {
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
@@ -19,13 +20,19 @@ class MovieLibrary extends Component {
           bookmarkedOnly=""
           onBookmarkedChange=""
           selectedGenre=""
-          onSelectedGenreChange="" 
+          onSelectedGenreChange=""
         />
-        {/* <MovieList movies={ this.props.movies } /> */}
-        <AddMovie />
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieLibrary;
