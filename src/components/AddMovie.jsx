76d3767ef15608de21e0b1ee0d5676442e0a7b9e
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-04 18:05:43 -0300

    adicionando coisas

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovies extends Component {
   constructor() {
@@ -15,15 +16,14 @@ class AddMovies extends Component {
   }
 
   HandleSearch = ({ target }) => {
-    const { id, value } = target;
+    const { name, value } = target;
     this.setState({
-      [id]: value,
+      [name]: value,
     });
   }
 
-  HandleClick = (event) => {
+  HandleClick = () => {
     const { onClick } = this.props;
-    event.preventDefault();
     onClick(this.state);
     this.setState({
       subtitle: '',
@@ -35,47 +35,93 @@ class AddMovies extends Component {
     });
   }
 
+  AtribbuteTest = (name) => {
+    if (name === 'title') {
+      return ('title-input');
+    }
+    if (name === 'subtitle') {
+      return ('subtitle-input');
+    }
+    if (name === 'imagePath') {
+      return ('image-input');
+    }
+    if (name === 'storyline') {
+      return ('storyline-input');
+    }
+    if (name === 'rating') {
+      return ('rating-input');
+    }
+    if (name === 'genre') {
+      return ('genre-input');
+    }
+  }
+
+  AttributeTitleNew = (name, value, onChange, type) => {
+    if (type !== 'textarea') {
+      return (<input
+        name={ name }
+        value={ value }
+        onChange={ onChange }
+        type={ type }
+        data-testid={ this.AtribbuteTest(name) }
+      />);
+    }
+
+    return (<textarea
+      name={ name }
+      value={ value }
+      onChange={ onChange }
+      data-testid={ this.AtribbuteTest(name) }
+    />);
+  }
+
   render() {
     const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <label htmlFor="title" data-testid="title-input-label">
           Título
-          <input id="title" value={title} onChange={this.HandleSearch} data-testid="title-input" />
+          {this.AttributeTitleNew('title', title, this.HandleSearch, 'text')}
         </label>
         <label htmlFor="subtitle" data-testid="subtitle-input-label">
           Subtítulo
-          <input id="subtitle" value={subtitle} onChange={this.HandleSearch} data-testid="subtitle-input" />
+          {this.AttributeTitleNew('subtitle', subtitle, this.HandleSearch, 'text')}
         </label>
         <label htmlFor="imagePath" data-testid="image-input-label">
           Imagem
-          <input type="text" id="imagePath" value={imagePath} onChange={this.HandleSearch} data-testid="image-input" />
+          {this.AttributeTitleNew('imagePath', imagePath, this.HandleSearch, 'text')}
         </label>
         <label htmlFor="storyline" data-testid="storyline-input-label">
           Sinopse
-          <input id="storyline" value={storyline} onChange={this.HandleSearch} data-testid="storyline-input" />
+          {this.AttributeTitleNew('storyline', storyline, this.HandleSearch, 'textarea')}
         </label>
         <label htmlFor="rating" data-testid="rating-input-label">
           Avaliação
-          <input id="rating" type="number" value={rating} onChange={this.HandleSearch} data-testid="rating-input" />
+          {this.AttributeTitleNew('rating', rating, this.HandleSearch, 'number')}
         </label>
         <label htmlFor="genre" data-testid="genre-input-label">
           Gênero
-          <select id="genre" value={genre} onChange={this.HandleSearch} data-testid="genre-input">
+          <select
+            name="genre"
+            value={ genre }
+            onChange={ this.HandleSearch }
+            data-testid="genre-input"
+          >
             <option value="action" data-testid="genre-option">Ação</option>
             <option value="comedy" data-testid="genre-option">Comédia</option>
             <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
         </label>
-        <button
-          type="submmit"
-          data-testid="send-button"
-          onClick={this.HandleClick}>
+        <button type="button" data-testid="send-button" onClick={ this.HandleClick }>
           Adicionar filme
         </button>
       </form>
     );
   }
 }
+
+AddMovies.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovies;
