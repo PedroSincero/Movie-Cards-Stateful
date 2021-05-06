c2a48839993e1bd23a8799c1ba9ded7e51e422ba
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-06 14:04:25 -0300

    adicionando comentarios

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,14 +5,16 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre:
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+
+  //   // this.state = {
+  //   //   searchText: '',
+  //   //   bookmarkedOnly: false,
+  //   //   selectedGenre: 
+  //   // };
+
+  // }
 
   HandleTest = () => {
 
